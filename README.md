04d51ebf6dbf3b54c1336e3362b491d7c5073356
Jonas Ribeiro <jonasribeiro19@gmail.com>
2025-07-21 14:50:13 -0300

    readme

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 
 # Api Multiagents
 
+Para vizualizar o swagger acesse [swagger](https://cdn2.multiagents.app/api/index.html) ou baixe este repositório e rode:
+
+Se tiver node na máquina.
+
+````sh
+node index.js
+```
+
+Ou se tiver php na máquina.
+
+````sh
+php -S localhost:8080
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 8080;
+
+http.createServer((req, res) => {
+  if (req.url === '/') {
+      const filePath = path.join(__dirname, 'index.html');
+      fs.readFile(filePath, (err, content) => {
+        if (err) {
+          res.writeHead(500);
+          res.end('Erro interno no servidor');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content);
+        }
+      });
+  } else if (req.url === '/resources/openapi.yaml') {
+      const filePath = path.join(__dirname, '/resources/openapi.yaml');
+      fs.readFile(filePath, (err, content) => {
+        if (err) {
+          res.writeHead(500);
+          res.end('Erro interno no servidor');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/yaml' });
+          res.end(content);
+        }
+      });
+  }
+
+}).listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
